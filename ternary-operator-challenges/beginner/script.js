7efeab9eb38d7c128791955b51d0670cbe6ebd1f
nhlanhlaMT <nhlanhla.mt88@gmail.com>
2025-05-19 09:33:56 +0200

    JavaScript Challenges

---
+++ ternary-operator-challenges/beginner/script.js
@@ -0,0 +1,9 @@
+// Challenge 1: Check if number is positive or negative
+
+function checkSign(num) {
+  return num > 0 ? "Positive" : "Negative or Zero";
+}
+
+console.log(checkSign(6));
+console.log(checkSign(0));
+console.log(checkSign(-1));
