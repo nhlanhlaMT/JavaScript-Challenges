f5c4b6992c9d5ae32083ff40ed0404da737d5925
nhlanhlaMT <nhlanhla.mt88@gmail.com>
2025-08-13 09:40:51 +0200

    completed beginner for loops challenges

---
+++ loops-challenges/for-loops/beginner-for-loops/script.js
@@ -0,0 +1,26 @@
+// Beginner for loop challenges
+
+// Task 1: Count from 1 to 10
+// Use a for loop to log numbers 1 through 10 to the console.
+
+// for (let count = 1; count <= 10; count++) {
+//   alert(count);
+// }
+
+// Task 2: Even Numbers Only
+// Print only even numbers between 1 and 20.
+
+// for (even = 2; even <=20; even += 2) {
+//   alert(even);
+// }
+
+// Task 3: Sum of Numbers 1–100
+// Use a for loop to add up numbers from 1 to 100 and show the total.
+
+let sum = 0
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+}
+
+alert("The sum of numbers from 1 to 100 is: " + sum);
