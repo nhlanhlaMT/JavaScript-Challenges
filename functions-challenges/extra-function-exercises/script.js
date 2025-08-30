e88323e09c58efedd2f5861f2e03281825dc29bc
nhlanhlaMT <nhlanhla.mt88@gmail.com>
2025-08-30 23:34:41 +0200

    extra function exercises

---
+++ functions-challenges/extra-function-exercises/script.js
@@ -0,0 +1,30 @@
+// The following function returns true if the parameter age is greater than 18.
+// Otherwise it asks for a confirmation and returns its result.
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Did parents allow you?');
+//   }
+// }
+
+// Rewrite it, to perform the same, but without if, in a single line.
+// Make two variants of checkAge:
+// Using a question mark operator ?
+// Using OR ||
+
+
+// My solution using a question mark operator ?
+// function checkAge(age) {
+//   return age > 18 ? true : confirm("Did parents allow you?");
+// }
+
+// alert(checkAge(19));
+
+// My solution OR ||
+function checkAge(age) {
+  return (age > 18) || confirm("Did parents allow you?");
+}
+
+alert(checkAge(19));
