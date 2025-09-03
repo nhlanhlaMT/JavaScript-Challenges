95345158545f8e3f948ce8f6c0507ed136068349
nhlanhlaMT <nhlanhla.mt88@gmail.com>
2025-09-03 18:03:30 +0200

    completed extra function exercise

--- functions-challenges/extra-function-exercises/script.js
+++ functions-challenges/extra-function-exercises/script.js
@@ -1,4 +1,4 @@
-// The following function returns true if the parameter age is greater than 18.
+// Task 1: The following function returns true if the parameter age is greater than 18.
 // Otherwise it asks for a confirmation and returns its result.
 
 // function checkAge(age) {
@@ -21,7 +21,7 @@ function checkAge(age) {
   return age > 18 ? true : confirm("Did parents allow you?");
 }
 
-alert(checkAge(17));
+// alert(checkAge(17));
 
 // My solution OR ||
 
@@ -30,3 +30,18 @@ function checkAge(age) {
 }
 
 alert(checkAge(19));
+
+// Task 2: Function min(a, b)
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// For instance:
+
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+alert(min(3, 2));
