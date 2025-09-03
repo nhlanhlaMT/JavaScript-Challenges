d714d3a1ccd90c232e0f21008dfd39984491b5d4
nhlanhlaMT <nhlanhla.mt88@gmail.com>
2025-09-03 18:48:47 +0200

    extra function exercise

--- functions-challenges/extra-function-exercises/script.js
+++ functions-challenges/extra-function-exercises/script.js
@@ -45,3 +45,31 @@ function min(a, b) {
 }
 
 alert(min(3, 2));
+
+// Task 3: Function pow(x,n)
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// Create a web-page that prompts for x and n, and then shows the result of pow(x,n).
+
+//solution from javascript.info
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Power ${n} is not supported, use a positive integer`);
+} else {
+  alert( pow(x, n) );
+}
